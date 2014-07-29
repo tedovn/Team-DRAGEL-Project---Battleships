7acf4626b8b9bcbb3fa58fba59b52a83737f8cad
Antoveravip <petar@petarivanov.com>
2014-07-29 23:51:22 +0300

    Adding computer move func
    Add computer move functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -127,6 +127,40 @@ function computerMove() {
   var x,y,pass;
   var sx,sy;
   var selected = false;
+  
+  /* Make two passes during 'shoot to kill' mode */
+  for (pass=0;pass<2;++pass) {
+    for (y=0;y<gridy && !selected;++y) {
+      for (x=0;x<gridx && !selected;++x) {
+        /* Explosion shown at this position */
+        if (player[y][x][0]==103) {
+          sx=x; sy=y;
+          var nup=(y>0 && player[y-1][x][0]<=100);
+          var ndn=(y<gridy-1 && player[y+1][x][0]<=100);
+          var nlt=(x>0 && player[y][x-1][0]<=100);
+          var nrt=(x<gridx-1 && player[y][x+1][0]<=100);
+          if ( pass == 0 ) {
+            /* On first pass look for two explosions in a row - next shot will be inline */
+            var yup=(y>0 && player[y-1][x][0]==103);
+            var ydn=(y<gridy-1 && player[y+1][x][0]==103);
+            var ylt=(x>0 && player[y][x-1][0]==103);
+            var yrt=(x<gridx-1 && player[y][x+1][0]==103);
+            if ( nlt && yrt) { sx = x-1; selected=true; }
+              else if ( nrt && ylt) { sx = x+1; selected=true; }
+              else if ( nup && ydn) { sy = y-1; selected=true; }
+              else if ( ndn && yup) { sy = y+1; selected=true; }
+            } else {
+              /* Second pass look for single explosion - fire shots all around it */
+              if ( nlt ) { sx=x-1; selected=true; }
+              else if ( nrt ) { sx=x+1; selected=true; }
+              else if ( nup ) { sy=y-1; selected=true; }
+              else if ( ndn ) { sy=y+1; selected=true; }
+            }
+          }
+        }
+      }
+    }
+// TODO
 }
 
 /* When whole ship is hit, show it using changed graphics
